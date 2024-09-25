02bc600576c6fcec56bc20cc66f41b1702912acf
Tarun sisodia <coccoder999@gmail.com>
2024-09-25 10:39:57 +0530

    Updated main

--- checkup2/lib/main.dart
+++ checkup2/lib/main.dart
@@ -4,6 +4,28 @@ import 'package:checkup2/base/bottom_navigation_bar.dart';
 void main() {
   runApp(const MyApp());
 }
+//concept of required.
+/*
+  var run = app(x: 2, y: 5);
+  print(run.x);
+
+  var run1 = app(x: 2, y: 5);
+
+  print(run1.y);
+
+class app {
+  int x;
+  int y;
+
+  app({required this.x, required this.y});
+
+  //copy the whole class.
+  app copyWith({int? x, int? y}) {
+    //Returning the App.
+    return app(x: x ?? this.x, y: y ?? this.y);
+  }
+}
+*/
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

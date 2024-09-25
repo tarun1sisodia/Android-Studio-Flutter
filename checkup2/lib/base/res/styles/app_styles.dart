37784fa7818499617ead8ef91decde1cc4796355
Tarun sisodia <coccoder999@gmail.com>
2024-09-25 10:37:51 +0530

    TextStyles for global use

--- checkup2/lib/base/res/styles/app_styles.dart
+++ checkup2/lib/base/res/styles/app_styles.dart
@@ -5,17 +5,34 @@ Color primary = const Color(0xff4768b6);
 class AppStyles {
   static Color primarycolor = primary;
   static Color textcolor = const Color(0xff3b3b3b);
+  static Color textblue = const Color(0xff4396a9);
 
   //Text Style for normal
   static TextStyle textStyle = TextStyle(
       fontWeight: FontWeight.w400,
-      fontSize: 17,
+      fontSize: 16,
       color: textcolor,
       fontFamily: 'Roboto');
+
   //Text Style for 2nd.
   static TextStyle textStyle1 = TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 20,
       color: textcolor,
       fontFamily: 'Roboto');
+
+  //Text Style for messages
+
+  static TextStyle textStyle2 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    color: primarycolor,
+    // fontFamily: 'Roboto'
+  );
+  static TextStyle textStyle3 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    color: Colors.white,
+    // fontFamily: 'Roboto'
+  );
 }

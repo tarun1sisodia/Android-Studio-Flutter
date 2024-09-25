326a90a241e3d1dafa4b2a3394f2d6cb1ebb59f4
Tarun sisodia <coccoder999@gmail.com>
2024-09-25 13:27:58 +0530

    25% ticket Container Completed for upcoming tickets.

--- checkup2/lib/base/widgets/ticket_view.dart
+++ checkup2/lib/base/widgets/ticket_view.dart
@@ -1,5 +1,6 @@
+import 'package:checkup2/base/widgets/app_lay_build.dart';
+import 'package:checkup2/base/widgets/big_dot.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 import '../res/styles/app_styles.dart';
 
@@ -11,38 +12,67 @@ class TicketView extends StatelessWidget {
     //it will take the size of whole screen.
     final size = MediaQuery.of(context).size;
 
-    print(size.height);
-    print(size.width);
+    print("Ticket View Size of height ${size.height.floor()}");
+    print("Ticket View Size of Width ${size.width.floor()}");
 
     return SizedBox(
       width: size.width * 1,
       height: 179,
       child: Container(
-        margin: EdgeInsets.only(right: 25, left: 20),
-        padding: EdgeInsets.all(13),
+        margin: const EdgeInsets.only(right: 25, left: 20),
+        padding: const EdgeInsets.all(11),
+        // color: Colors.yellow,
+        decoration: BoxDecoration(
+            color: AppStyles.textblue,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20))),
         child: Column(
           children: [
             Row(
               children: [
                 Text(
                   "Agra",
+
+                  //calling App_styles for text style
                   style: AppStyles.textStyle3,
                 ),
+                //Creating Space Between Text and Big Dots.
+                Expanded(child: Container()),
+
+                //Calling BigDots.
+                const BigDot(),
 
                 //It will expand size between text.
+                const Expanded(
+                    child: Stack(
+                  children: [
+                    //Maintain Space between both devices.
+
+                    SizedBox(
+                      height: 24,
+                      //Calling App layout builder for Dots.
+                      child: AppLayBuild(
+                        randSize: 7,
+                      ),
+                    ),
+
+                    Center(
+                      child: Icon(CupertinoIcons.airplane),
+                    )
+                  ],
+                )),
+
+                const BigDot(),
+                //Creating Space Between Text and Big Dots.
                 Expanded(child: Container()),
-                Text("Delhi", style: AppStyles.textStyle3)
+                Text("Delhi", style: AppStyles.textStyle3),
               ],
             ),
           ],
         ),
-        // color: Colors.yellow,
-        decoration: BoxDecoration(
-            color: AppStyles.textblue,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            )),
       ),
     );
   }

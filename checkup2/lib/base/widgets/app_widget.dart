5110545357cd91415cceaedb6663dda256c7bb8f
Tarun sisodia <coccoder999@gmail.com>
2024-09-25 10:38:50 +0530

    Messages for User by custom widgets

--- checkup2/lib/base/widgets/app_widget.dart
+++ checkup2/lib/base/widgets/app_widget.dart
@@ -2,26 +2,36 @@ import 'package:checkup2/base/res/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class AppDoubleText extends StatelessWidget {
-  AppDoubleText({super.key, required this.bigtxt, required this.smalltxt});
+  const AppDoubleText(
+      {super.key, required this.bigtxt, required this.smalltxt});
+  //String for Text on display.
   final String bigtxt;
   final String smalltxt;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          bigtxt,
-          style: AppStyles.textStyle1,
-        ),
-        InkWell(
-          onTap: () {},
-          child: Text(
-            smalltxt,
+    return Container(
+      //Padding For Text
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        //Space between text
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            bigtxt,
             style: AppStyles.textStyle1,
           ),
-        )
-      ],
+          //What is InkWell--> A Widget which calls Function onTap.
+          InkWell(
+            onTap: () {},
+            //For function.
+            child: Text(
+              smalltxt,
+              //copywith is used to copy all from one and alter/ Addcurrentone design.
+              style: AppStyles.textStyle2.copyWith(color: Colors.blueAccent),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

c432bd0e2cabf0a9486459d33282d7ce29199c90
Tarun sisodia <coccoder999@gmail.com>
2024-09-26 15:38:42 +0530

    Dynamically Count the Dashes for Ticket.

--- checkup2/lib/base/widgets/app_lay_build.dart
+++ checkup2/lib/base/widgets/app_lay_build.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppLayBuild extends StatelessWidget {
   //Randome Size Generator.
   final int randSize;
-  const AppLayBuild({super.key, required this.randSize});
+  final double width;
+  const AppLayBuild({super.key, required this.randSize, this.width = 4});
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +21,14 @@ class AppLayBuild extends StatelessWidget {
 
           //Hard to understand
           children: List.generate(
-              (constraints.constrainWidth() / randSize).floor(),
-              (index) => const SizedBox(
-                    width: 4,
-                    height: 2,
-                    child: DecoratedBox(
-                        decoration: BoxDecoration(color: Colors.white)),
-                  )));
+            (constraints.constrainWidth() / randSize).floor(),
+            (index) => SizedBox(
+              child: Container(
+                  width: width,
+                  height: 2,
+                  decoration: const BoxDecoration(color: Colors.white)),
+            ),
+          ));
     });
   }
 }

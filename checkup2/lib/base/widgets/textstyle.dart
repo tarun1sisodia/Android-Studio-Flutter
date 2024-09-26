866f33e4fc123a42ca7f563378b4e431d247731f
Tarun sisodia <coccoder999@gmail.com>
2024-09-26 15:32:08 +0530

    Creates for custom text design

---
+++ checkup2/lib/base/widgets/textstyle.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../res/styles/app_styles.dart';
+
+class Textstyle extends StatelessWidget {
+  //Var declared to access
+  final String text;
+  final TextAlign align;
+  //Named Parameters
+  const Textstyle(
+      {super.key, required this.text, this.align = TextAlign.start});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: align,
+      //calling App_styles for text style
+      style: AppStyles.textStyle2.copyWith(color: Colors.white),
+    );
+  }
+}

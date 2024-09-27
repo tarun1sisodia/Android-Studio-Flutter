fcaf58359ad8daa80303593e7a3ccf9af5b195a5
Tarun sisodia <coccoder999@gmail.com>
2024-09-28 00:29:58 +0530

    All Tickets are stored In New Screen

--- checkup2/lib/base/widgets/app_widget.dart
+++ checkup2/lib/base/widgets/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:checkup2/Screens/all_tickets.dart';
 import 'package:checkup2/base/res/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,16 @@ class AppDoubleText extends StatelessWidget {
           ),
           //What is InkWell--> A Widget which calls Function onTap.
           InkWell(
-            onTap: () {},
+            //On Tapping the View All
+            onTap: () {
+              //it will open a new screen from right to left.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    //AllTickets is a dart file.
+                    builder: (BuildContext context) => const AllTickets()),
+              );
+            },
             //For function.
             child: Text(
               smalltxt,

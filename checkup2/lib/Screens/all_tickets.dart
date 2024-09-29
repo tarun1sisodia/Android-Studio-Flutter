9566e04af10b9b3b078093d1fcf9abce0e9b23c6
Tarun sisodia <coccoder999@gmail.com>
2024-09-29 12:28:58 +0530

    Add Tickets To new screen

--- checkup2/lib/Screens/all_tickets.dart
+++ checkup2/lib/Screens/all_tickets.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../base/widgets/ticket_view.dart';
+
 class AllTickets extends StatelessWidget {
   const AllTickets({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text(
+          "All Tickets",
+          style: TextStyle(fontSize: 20),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: const [
+          Column(
+            children: [
+              TicketView(),
+              TicketView(),
+              TicketView(),
+              TicketView(),
+              TicketView(),
+              TicketView(),
+              TicketView(),
+              TicketView(),
+            ],
+          )
+        ],
+      ),
     );
   }
 }

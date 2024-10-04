0257030016aed585865e105ab1e8e3b1e85fd411
Tarun sisodia <coccoder999@gmail.com>
2024-10-04 13:32:53 +0530

    Hotels Data in json

---
+++ checkup2/lib/base/utils/hotels_json.dart
@@ -0,0 +1,76 @@
+List<Map<String, dynamic>> hotellist = [
+  {
+    'image': 'hotel1.jpg',
+    'place': 'Taj Hotel',
+    'destination': 'Agra',
+    'price': 1000
+  },
+  {
+    'image': 'hotel2.jpg',
+    'place': 'Taj Hotel',
+    'destination': 'Mathura',
+    'price': 1500
+  },
+  {
+    'image': 'hotel3.jpg',
+    'place': 'Sanjay Place',
+    'destination': 'Agra',
+    'price': 3000
+  },
+  {
+    'image': 'hotel4.jpg',
+    'place': 'Agravan Hotel',
+    'destination': 'Agra',
+    'price': 5000
+  },
+  {
+    'image': 'hotel_bed1.jpg',
+    'place': 'Taj Hotel',
+    'destination': 'Agra',
+    'price': 1000
+  },
+  {
+    'image': 'hotel_bed2.jpg',
+    'place': 'Night Place',
+    'destination': 'Delhi',
+    'price': 10300
+  },
+  {
+    'image': 'hotel_stairs4.jpg',
+    'place': 'Hotel Sec',
+    'destination': 'Agra',
+    'price': 10000
+  },
+];
+List<Map<String, dynamic>> Planelist = [
+  {
+    'image': 'plane1.jpg',
+    'place': 'American Express',
+    'destination': 'Delhi',
+    'price': 10000
+  },
+  {
+    'image': 'plane2.jpg',
+    'place': 'Reliance',
+    'destination': 'Mathura',
+    'price': 15000
+  },
+  {
+    'image': 'plane3.jpg',
+    'place': 'Kolkata',
+    'destination': 'Agra',
+    'price': 3000
+  },
+  {
+    'image': 'plane4.jpg',
+    'place': 'Chennai',
+    'destination': 'Tokyo',
+    'price': 50000
+  },
+  {
+    'image': 'plane5.jpg',
+    'place': 'Columbo',
+    'destination': 'Jewar',
+    'price': 50000
+  },
+];

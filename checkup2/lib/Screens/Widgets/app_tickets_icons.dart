c921c12638b8b08b35b4b4972589d5556fedd689
Tarun sisodia <coccoder999@gmail.com>
2024-10-08 17:30:55 +0530

    Launch App

--- checkup2/lib/Screens/Widgets/app_tickets_icons.dart
+++ checkup2/lib/Screens/Widgets/app_tickets_icons.dart
@@ -12,11 +12,22 @@ class AppTicketsIcons extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10), color: Colors.white),
-      child: Row(
+      child: Column(
         children: [
-          const Icon(CupertinoIcons.airplane, color: Color(0xFF9496C3)),
-          SizedBox(width: 10),
-          Text("Departure", style: AppStyles.textStyle)
+          Row(
+            children: [
+              const Icon(CupertinoIcons.airplane, color: Color(0xFF9496C3)),
+              SizedBox(width: 10),
+              Text("Departure", style: AppStyles.textStyle)
+            ],
+          ),
+          Row(
+            children: [
+              const Icon(CupertinoIcons.airplane, color: Color(0xFF9496C3)),
+              SizedBox(width: 10),
+              Text("Departure", style: AppStyles.textStyle)
+            ],
+          ),
         ],
       ),
     );

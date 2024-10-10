e030445be22f577acdf2f6912f9c3f6881fdf7d4
Tarun sisodia <coccoder999@gmail.com>
2024-10-10 18:35:20 +0530

    Login Page

--- checkup2/lib/Screens/profile.dart
+++ checkup2/lib/Screens/profile.dart
@@ -1,5 +1,8 @@
+import 'package:checkup2/base/login_&_Registration/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../base/res/styles/app_styles.dart';
 
 class profile extends StatefulWidget {
   const profile({super.key});
@@ -9,18 +12,35 @@ class profile extends StatefulWidget {
 }
 
 class _profileState extends State<profile> {
-  void page() {
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile"),
-        leading: const Icon(CupertinoIcons.person),
-      ),
+          title: const Text("Profile"),
+          leading: const Icon(CupertinoIcons.person)),
       backgroundColor: const Color(0xFFEFD2D2),
+      body: Container(
+        child: InkWell(
+          //On Tapping the View All
+          onTap: () {
+            //it will open a new screen from right to left.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                //AllTickets is a dart file.
+                builder: (BuildContext context) => const Login(),
+              ),
+            );
+          },
+          //For function.
+          child: Center(
+            child: Text(
+              "Login",
+              style: AppStyles.textStyle1,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

d4d0ba1a54862ee0d9e85d82364e1b64730446d2
Tarun sisodia <coccoder999@gmail.com>
2024-10-10 18:29:40 +0530

    build and gitignore are unset

---
+++ checkup2/lib/Screens/Widgets/find_tickets.dart
@@ -0,0 +1,24 @@
+import 'package:checkup2/base/res/styles/app_styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FindTickets extends StatelessWidget {
+  const FindTickets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+      decoration: BoxDecoration(
+          color: AppStyles.orange, borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: () {},
+        child: Center(
+            child: Text(
+          "Find Tickets",
+          style: AppStyles.textStyle1.copyWith(fontWeight: FontWeight.w500),
+        )),
+      ),
+    );
+  }
+}

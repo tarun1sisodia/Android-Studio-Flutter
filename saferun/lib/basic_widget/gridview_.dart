342d032505765f510c59a76767dceead9bf32d5e
Tarun sisodia <coccoder999@gmail.com>
2024-10-13 16:14:24 +0530

    Grid View

--- saferun/lib/basic_widget/gridview_.dart
+++ saferun/lib/basic_widget/gridview_.dart
@@ -5,10 +5,10 @@ class GridVieW extends StatefulWidget {
   const GridVieW({super.key});
 
   @override
-  State<GridVieW> createState() => _LimitsOfContainerState();
+  State<GridVieW> createState() => _GridVieWState();
 }
 
-class _LimitsOfContainerState extends State<GridVieW> {
+class _GridVieWState extends State<GridVieW> {
   List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   List names = ["Tarun", "Sanjay", "Prashant", "Yogita", "Kashish"];
